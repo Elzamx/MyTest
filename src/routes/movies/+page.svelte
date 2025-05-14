fac8fe68fcba01dc822f23cfc4578b87c58dc58c
Elzamx <miftaelz@students.zhaw.ch>
2025-05-14 13:40:00 +0200

    first commit

---
+++ src/routes/movies/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import MovieCard from "$lib/components/MovieCard.svelte";
+  let { data } = $props();
+</script>
+
+<p><i>Daten und Bilder generiert mit ChatGPT und DALL-E</i></p>
+<div class="row">
+  {#each data.movies as movie}
+    <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
+      <MovieCard {movie}></MovieCard>
+    </div>
+  {/each}
+</div>
